9bc3d74a5b7b5d3e1ccebeade2ec3ba932f2c171
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2020-09-19 21:33:59 +0200

    setup apollo client

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,47 @@
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from "@apollo/client";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import Pages from "./pages";
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  // headers: { authorization: localStorage.getItem('token') || ''},
+  cache: new InMemoryCache()
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // test query
+// client.mutate({
+//   mutation: gql`
+//     mutation TestQuery {
+//       login(email:"aa@aa", password:"123") {
+//         success,
+//         resMessage
+//         token
+//       }
+//     }
+//   `
+// }).then(result => console.log(result));
+
+
+// ReactDOM.render(
+//   <ApolloProvider client={client}>
+//     <Pages />
+//   </ApolloProvider>,
+//   document.getElementById("root")
+// );
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
